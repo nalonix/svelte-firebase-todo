f64d897c1f999a7dca9c58857a38152626e2d43d
Naol Fekadu <88847818+doniverse@users.noreply.github.com>
2023-10-14 10:39:50 +0300

    Update firebase.js

--- src/lib/firebase/firebase.js
+++ src/lib/firebase/firebase.js
@@ -7,12 +7,12 @@ import {getAuth} from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: AIzaSyCR3uHo2tsRHg4Yb5OwHjXgWz17PXyp03s,
-    authDomain: svelte-crud-ffb46.firebaseapp.com,
-    projectId: svelte-crud-ffb46,
-    storageBucket: svelte-crud-ffb46.appspot.com,
-    messagingSenderId: 851428174314,
-    appId: 1:851428174314:web:3e60cf25df16ebbe51aa93
+    apiKey: "AIzaSyCR3uHo2tsRHg4Yb5OwHjXgWz17PXyp03s",
+  authDomain: "svelte-crud-ffb46.firebaseapp.com",
+  projectId: "svelte-crud-ffb46",
+  storageBucket: "svelte-crud-ffb46.appspot.com",
+  messagingSenderId: "851428174314",
+  appId: "1:851428174314:web:3e60cf25df16ebbe51aa93"
 };
 
 // Initialize Firebase
